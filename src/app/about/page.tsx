47d6a20399c6c12011fd3c5bb2c009c1e6096633
PixMusicaX <pinakipps21@gmail.com>
2026-05-09 16:18:12 +0530

    credits

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -418,7 +418,7 @@ export default function AboutPage() {
       {/* Footer */}
       <div className="pt-12 text-center space-y-4 border-t border-tm-blue-gray/10">
         <p className="text-xs font-black uppercase text-tm-blue-gray tracking-[0.3em]">
-          Version 2.5.3 • © 2026 TaskMaster
+          Version 2.5.4 • © 2026 TaskMaster • By Pinaki AKA PiX
         </p>
       </div>
     </div>
